7c04e7d987de4734564c0404bbbaa6563a92d1f5
rajpatelbot <rajpatel158g@gmail.com>
2024-09-30 00:40:47 +0530

    chore: Removed unused code

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,10 +1,4 @@
-'use client';
-
-import { useState } from 'react';
-
 export default function Home() {
-   const [showAlert, setShowAlert] = useState(false);
-
    return (
       <main className='flex min-h-screen flex-col items-center justify-center p-24'>
          <p>Hello</p>
